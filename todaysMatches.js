0af92ec73105012bee595170fec4ef920d8cd1f8
smuke <smuke@smuke.dev>
2024-12-01 12:48:24 -0800

    Remove newline

--- todaysMatches.js
+++ todaysMatches.js
@@ -1,19 +1,17 @@
 document.querySelectorAll(".teambox .middleExtra").forEach(e => {
-    e.style.lineHeight = "1.5";
-
     const [hours, mins] = e.innerText.split(":");
 
     if (hours > 12) {
-        e.innerHTML = `${hours - 12}:${mins}<br>PM`;
+        e.innerText = `${hours - 12}:${mins} PM`;
     }
     else if (hours == 0) {
-        e.innerHTML = `12:${mins}<br>AM`;
+        e.innerText = `12:${mins} AM`;
     }
     else if (hours == 12) {
-        e.innerHTML = `12:${mins}<br>PM`;
+        e.innerText = `12:${mins} PM`;
     }
     else {
-        if (hours < 10) e.innerHTML = `${hours.substring(1)}:${mins}<br>AM`;
-        else e.innerHTML = `${hours}:${mins}<br>AM`;
+        if (hours < 10) e.innerHTML = `${hours.substring(1).toString()}:${mins.toString()}\nAM`;
+        else e.innerText = `${hours}:${mins} AM`;
     }
 });
