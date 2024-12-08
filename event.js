a8b8aef272a417a80dda8f1c14908956d42fc3e6
Ethel Arterberry <8540373+soops@users.noreply.github.com>
2024-12-08 15:17:26 -0800

    Added event page playoff times

---
+++ event.js
@@ -0,0 +1,33 @@
+function updateEventPlayoffTimes() {
+    document.querySelectorAll(".time-time").forEach(e => {
+        const [hours, mins] = e.innerText.split(":");
+        console.log(hours, mins);
+
+        if (hours > 12) {
+            e.innerText = `${hours - 12}:${mins} PM`;
+        }
+        else if (hours == 0) {
+            e.innerText = `12:${mins} AM`;
+        }
+        else if (hours == 12) {
+            e.innerText = `12:${mins} PM`;
+        }
+        else {
+            if (hours < 10) e.innerText = `${hours.substring(1)}:${mins} AM`;
+            else e.innerText = `${hours}:${mins} AM`;
+        }
+    });
+}
+
+const matchObserver = new MutationObserver((mutations, observer) => {
+    const targetText = mutations[0].target.textContent.toLowerCase();
+    if (targetText.includes("am") || targetText.includes("pm")) {
+        observer.disconnect();
+        return;
+    }
+    
+    updateEventPlayoffTimes();
+});
+
+updateEventPlayoffTimes();
+matchObserver.observe(document.querySelector(".time-time"), { childList: true });
